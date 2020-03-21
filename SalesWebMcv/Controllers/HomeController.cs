127a0da2a3327026d443e1845e9eae9f2fc1be77
Phviana <paulohenrique_viana@hotmail.com>
2020-03-21 18:23:21 -0300

    First Model-Controller-View - Department

--- SalesWebMcv/Controllers/HomeController.cs
+++ SalesWebMcv/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMcv.Models;
+using SalesWebMcv.Models.ViewModels;
 
 namespace SalesWebMcv.Controllers
 {
