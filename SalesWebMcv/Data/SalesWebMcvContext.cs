5ee0bf95f1924b3dee0dd4e2ad6e3a1494e22ace
Phviana <paulohenrique_viana@hotmail.com>
2020-03-21 19:18:23 -0300

    MySQL adaptation and first migration

---
+++ SalesWebMcv/Data/SalesWebMcvContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMcv.Models;
+
+namespace SalesWebMcv.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMcv.Models.Department> Department { get; set; }
+    }
+}
